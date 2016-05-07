ad80cd8855e1a539a0763ead08f148b1c431b32b
Ward Van Assche <ward.v.assche@gmail.com>
2016-05-07 15:05:56 +0200

    Verder geschreven aan scriptie

--- SignalSync/src/be/signalsync/test/RecordedTest.java
+++ SignalSync/src/be/signalsync/test/RecordedTest.java
@@ -23,14 +23,14 @@ import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
 public class RecordedTest {
 	private final static int SAMPLE_RATE = 8000;
 	private final static int NFFT_BUFFER_SIZE = 512;
-	private final static int NFFT_STEP_SIZE = 128;
+	private final static int NFFT_STEP_SIZE = 256;
 	
 	private final static int NUMBER_OF_SLICES = 27;
 	private final static int NUMBER_OF_RECORDINGS = 3;
 	
 	private final static String REFERENCE_TEMPLATE = "./Slices/Recorded/opname-reference.wav - slice - %d.wav";
 	private final static String OTHER_TEMPLATE = "./Slices/Recorded/opname-%d.wav - slice - %d.wav";
-	private final static double[] LATENCIES = { -2.391D, -1.847D, -4.052D };
+	private final static double[] LATENCIES = { 2.391D, 1.847D, 4.052D };
 	private final static String[] RECORDING_TYPE = {"goeie microfoon via geluidskaart", 
 			"goeie microfoon zonder geluidskaart", "slechte microfoon"};
 	
